fde517809884ecfe27c1abeedca9f5ac757a9adc
Umesh Pal <umeshpal.github@gmail.com>
2026-09-13 12:18:03 +0530

    MergeSort

---
+++ MergeSort.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+public class MergeSort {
+    // Merge Step-Function
+    public static void merge(List<Integer> arr, int st, int mid, int end) {
+
+    }
+
+    public static void mergeSort {
+    }
+
+    public static void main(String[] args) {
+    List<Integer> arr = new ArrayList<>(Arrays.asList(12 ,31 ,35 ,8 ,32, 17));
+    mergeSort(arr,0,arr.size()-1);
+    for (int val : arr) {
+        Syso
+    }
+
+
+}
+
+}
