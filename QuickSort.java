ca347b7a12fa87bfcbf0d13edf0619f23b564ef0
Umesh Pal <umeshpal.github@gmail.com>
2026-09-16 17:40:52 +0530

    QuickSort

--- QuickSort.java
+++ QuickSort.java
@@ -1,47 +1,50 @@
 public class QuickSort {
 
     // Partition Function
+    // All elements Smaller than Or equal to the pivot are moved To its left, and
+    // all elements Greater are moved to its right.
     public static int partition(int[] arr, int st, int end) {
-        int idx = st - 1;
-        int pivot = arr[end];
+        int idx = st - 1; // idx tracks the boundary of elements smaller than or equal to the pivot.
+        int pivot = arr[end]; // Choose the last element of the current subarray as the pivot.
 
+        // Loop through the subarray from start to end-1
         for (int j = st; j < end; j++) {
             if (arr[j] <= pivot) { // Comparisions
-                idx++;
+                idx++; // Expand the region of smaller elements
 
-                // Swap arr[idx] with arr[j]
-                int[] temp = arr[idx];
+                // Swap the Smaller element at 'j' with the element at 'idx'
+                int temp = arr[idx];
                 arr[idx] = arr[j];
                 arr[j] = temp;
             }
         }
-        // Swap arr[idx] with arr[end]
+
+        // Places the pivot element At its Correct sorted position.
+        idx++;
         int temp = arr[idx];
         arr[idx] = arr[end];
         arr[end] = temp;
 
+        // Return the partitioning index
         return idx;
     }
 
     // QuickSort Function
-    public static void QuickSort(arr, st, end) {
-        if (st < end) {
-            int pivotIdx = partition(arr, st, end);
-            QuickSort(arr, st, pivIdx - 1); //Left Half
-            QuickSort(arr, pivIdx + 1, end); //Right Half
-            
+    public static void quickSort(int[] arr, int st, int end) {
+        if (st < end) { // Base case: Check if the subarray has more than one element.
+
+            int pivotIdx = partition(arr, st, end); // Find the pivot index that elements are correctly partitioned.
+            quickSort(arr, st, pivotIdx - 1); // Recursively sort the left subarray.
+            quickSort(arr, pivotIdx + 1, end); // Recursively sort the right subarray.
         }
     }
 
     // Driver Function
     public static void main(String[] args) {
-        int[] arr = { 12, 31, 35, 8, 32, 17 };
-        QuickSort(arr, 0, arr.length() - 1);
+        int[] arr = { 12, 31, 35, 8, 32, 17 }; // Unsorted array
 
-        for (int val : arr) {
-            System.out.print(" ");
-        }
-        System.out.println();
-        return 0;
+        System.out.println("Original array : " + java.util.Arrays.toString(arr));
+        quickSort(arr, 0, arr.length - 1); // Call the quicksort function on the entire array
+        System.out.println("Sorted array : " + java.util.Arrays.toString(arr));
     }
 }
