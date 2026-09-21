905240eb8642190cc3d1f42123c63798a5b271ad
Umesh Pal <umeshpal.github@gmail.com>
2026-09-22 01:29:35 +0530

    New Changes

---
+++ 2D_Arrays_Problems/MultidimensionalArrays.java
@@ -0,0 +1,16 @@
+public class MultidimensionalArrays {
+    // Driver Function
+    public static void main(String[] args) {
+        int matrix[][] = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
+        int rows = 4;
+        int columns = 4;
+
+        // Nested Loops to print the matrix
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println(); // Moves to the next line
+        }
+    }
+}
